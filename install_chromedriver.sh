f3dd54e5cd9a4979ab9ca97f867d14cc8c1e152b
pepperic123 <pepperic123@gmail.com>
2025-02-27 13:51:14 +0100

    Create install_chromedriver.sh

---
+++ install_chromedriver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Installing Chrome and ChromeDriver..."
+
+# Install Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+# Install ChromeDriver
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -N "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+sudo mv -f chromedriver /usr/local/bin/
+rm chromedriver_linux64.zip
+
+echo "Chrome and ChromeDriver installed successfully."
