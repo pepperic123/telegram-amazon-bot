473990c31906387e5ca764da46d0aae590a5ad10
pepperic123 <pepperic123@gmail.com>
2025-02-28 09:36:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 TOKEN = "7213198162:AAHY9VfC-13x469C6psn3V36L1PGjCQxSs0"  # Inserisci il token del bot Telegram
 CHAT_ID = "-1002290458283"  # Inserisci l'username del tuo canale Telegram (es. "@OfferteAmazon")
-AMAZON_ACCESS_KEY = "AKPAV0YTNY1740423739"  # Inserisci la tua Amazon API Access Key
-AMAZON_SECRET_KEY = "g0N1qt9tB2AUB+chkTDjakR3nafgqmkGkfr77/2h"  # Inserisci la tua Amazon API Secret Key
+AWS_ACCESS_KEY = "AKPAV0YTNY1740423739"  # Inserisci la tua Amazon API Access Key
+AWS_SECRET_KEY = "g0N1qt9tB2AUB+chkTDjakR3nafgqmkGkfr77/2h"  # Inserisci la tua Amazon API Secret Key
 AMAZON_ASSOCIATE_TAG = "new1707-21"  # Il tuo tracking ID Amazon
 RAPIDAPI_KEY = "285c06ed85msh34004e8e5e52f0fp1b24d1jsnc07ccd8b5443"
 SCRAPERAPI_KEY = "73addc3cbaf02af137ac90f13d6f091f"
