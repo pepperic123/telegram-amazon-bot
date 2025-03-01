e675930ae746eb889e83b19b0c792cf72ef80100
pepperic123 <pepperic123@gmail.com>
2025-03-01 15:21:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option("useAutomationExtension", False)
 
 # User-Agent rotation
 user_agents = [
